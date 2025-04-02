2c66e6cdd6601c84778aaa293139df43a36539e9
levantini4n <hinfluenced@gmail.com>
2025-04-02 15:47:47 +0300

    Simplification of flake.nix

--- scene/flake.nix
+++ scene/flake.nix
@@ -1,6 +1,4 @@
 {
-  description = "Manim environment with system dependencies provided by Nix and Python packages managed by uv";
-
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
@@ -14,36 +12,7 @@
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
 	    manim
-            # ffmpeg-full
-            # texlive.combined.scheme-full
-            # python3
-            # ninja
-            # SDL
-            # SDL_gfx
-            # opencv
-            # cairo
-            # pango
-            # glib
-            # gobject-introspection
-            # librsvg
-            # zlib
-            # libGLU
-            # gtk2
-            # xorg.libXrender
-            # xorg.libXext
-            # ghostscript
-            # harfbuzz
-            # stdenv.cc.cc.lib
           ];
-
-          # LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath ([
-          #   pkgs.ffmpeg-full
-          #   pkgs.cairo
-          #   pkgs.pango
-          #   pkgs.glib
-          #   pkgs.librsvg
-          #   pkgs.stdenv.cc.cc.lib
-          # ])}";
 
           shellHook = ''
             # Set up a local directory for uv to install packages
